51628023cd0cd3ea3b96f8a199b4203c3186b37f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-27 15:52:51 +0200

    Updates

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  url: process.env.URL || 'https://klarspraak.micro.t-fk.win',
+  sitemapUrl: process.env.SITEMAP_URL || 'https://sitemap.t-fk.win?sitemap=https://www.telemark.no/sitemap.xml'
+}
